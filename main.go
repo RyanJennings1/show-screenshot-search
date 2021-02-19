ee99bf7b536f032102d60c6421e55bd52ac25ce8
Ryan Jennings <ryanjennings1010@gmail.com>
2021-02-19 23:38:17 +0000

    Extract video methods to video file

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import(
 	"fmt"
 	"os"
-	"os/exec"
-	"strings"
 )
 
 func main() {
@@ -40,40 +38,5 @@ Commands:
   -h, --help: prints this message
   -f, --file: the video file to run on
 	`)
-}
-
-func framesFromVideo(filename string) (string, error) {
-	fmt.Printf("Extracting frames from %s\n", filename)
-	ffmpegPath, err := exec.LookPath("ffmpeg")
-	if err != nil {
-		fmt.Printf("Error: FFMPEG was not found\n%s\n", err)
-		return "", err
-	}
-
-	fname := strings.Split(strings.Split(filename, "videos/")[1], ".")[0]
-	framesOutPath := "frames/" + fname + "%4d.png"
-	buildFfmpegCmd := &exec.Cmd {
-		Path: ffmpegPath,
-		Args: []string{
-			ffmpegPath,
-			"-ss",
-			"00:00:00",
-			"-i",
-			filename,
-			"-r",
-			"5.0",
-			framesOutPath,
-		},
-		Stdout: os.Stdout,
-		Stderr: os.Stdout,
-	}
-
-	fmt.Printf("Running command: %s\n", buildFfmpegCmd.String())
-	if err := buildFfmpegCmd.Run(); err != nil {
-		fmt.Printf("Error: FFMPEG command failed\n%s\n", err)
-		return "", err
-	}
-	fmt.Println("Frames generated")
-	return "", nil
 }
 
--- video.go
+++ video.go
@@ -1,9 +1,43 @@
-package video
+package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
 
-func video() {
-	fmt.Println("TODO")
+func framesFromVideo(filename string) (string, error) {
+	fmt.Printf("Extracting frames from %s\n", filename)
+	ffmpegPath, err := exec.LookPath("ffmpeg")
+	if err != nil {
+		fmt.Printf("Error: FFMPEG was not found\n%s\n", err)
+		return "", err
+	}
+
+	fname := strings.Split(strings.Split(filename, "videos/")[1], ".")[0]
+	framesOutPath := "frames/" + fname + "%4d.png"
+	buildFfmpegCmd := &exec.Cmd {
+		Path: ffmpegPath,
+		Args: []string{
+			ffmpegPath,
+			"-ss",
+			"00:00:00",
+			"-i",
+			filename,
+			"-r",
+			"5.0",
+			framesOutPath,
+		},
+		Stdout: os.Stdout,
+		Stderr: os.Stdout,
+	}
+
+	fmt.Printf("Running command: %s\n", buildFfmpegCmd.String())
+	if err := buildFfmpegCmd.Run(); err != nil {
+		fmt.Printf("Error: FFMPEG command failed\n%s\n", err)
+		return "", err
+	}
+	fmt.Println("Frames generated")
+	return "", nil
 }
